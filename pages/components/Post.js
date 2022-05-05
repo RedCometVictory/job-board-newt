477ba448197e46c7ee150b0c6f6c0dde0d5df658
unknown <acemiranda13@att.net>
2022-05-05 01:30:32 -0700

    time ago

--- pages/components/Post.js
+++ pages/components/Post.js
@@ -5,10 +5,10 @@ import ReactTimeAgo from 'react-time-ago';
 // import Image from "next/image";
 import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en.json'
-import ru from 'javascript-time-ago/locale/ru.json'
+// import ru from 'javascript-time-ago/locale/ru.json'
 
 TimeAgo.addDefaultLocale(en)
-TimeAgo.addLocale(ru)
+// TimeAgo.addLocale(ru)
 const Post = ({job}) => {
   let [initials, setIntitials] = useState(false);
   let { state, dispatch } = useAppContext();
