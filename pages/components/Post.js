48944ee68df8a2e1b4f7f2f676a22adae31cbd3b
unknown <acemiranda13@att.net>
2022-05-04 23:43:08 -0700

    job post edit

--- pages/components/Post.js
+++ pages/components/Post.js
@@ -32,20 +32,20 @@ const Post = ({job}) => {
       <div className="post__container">
         <div className="post__header">
           <div className="post__avatar">
-            {job.logo ? (
+            {job?.logo ? (
               <>
               <img
-                src={job.logo ? job.logo : ''}
+                src={job?.logo ? job.logo : ''}
                 alt="company logo" className="image"
                 onError={e => imgOnErrHandler(e)} 
               />
               <div className={`initials ${initials ? 'active' : ''}`}>
-                <p>{createInitials(job.company_name)}</p>
+                <p>{createInitials(job?.company_name)}</p>
               </div>
               </>
             ) : (
               <div className={`initials active`}>
-                <p>{createInitials(job.company_name)}</p>
+                <p>{createInitials(job?.company_name)}</p>
               </div>
             )}
           </div>
@@ -53,22 +53,22 @@ const Post = ({job}) => {
         <div className="post__description">
           <div className="post__desc-main">
             <div className="post__emp-name">
-              <h5 className="name">{job.company_name}</h5>
+              <h5 className="name">{job?.company_name}</h5>
             </div>
             <div className="post__position">
               <div>
                 <h3 className="title">
-                  <Link href={job.url}>{job.role}</Link>
+                  <Link href={job?.url}>{job?.role}</Link>
                 </h3>
               </div>
             </div>
             <div className="post__stats">
               <div>
-                <p><ReactTimeAgo date={Date.parse(job.date_posted)} locale="en-US"/></p>
+                <p><ReactTimeAgo date={Date.parse(job?.date_posted)} locale="en-US"/></p>
                 <p className="bullet">&bull;</p>
-                <p>{job.remote ? "Remote" : "Non-Remote"}</p>
+                <p>{job?.remote ? "Remote" : "Non-Remote"}</p>
                 <p className="bullet">&bull;</p>
-                <p className='location'>{job.location}</p>
+                <p className='location'>{job?.location}</p>
               </div>
             </div>
           </div>
